c6a810c97c29935298703f1c52cd86b15221ba82
Brent Yurek <brentagainagain@gmail.com>
2020-12-16 21:19:19 -0600

    Completed directory assignment

---
+++ Smoothstack_Assignments/src/com/smoothstack/assignments/day3/ListDir.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.smoothstack.assignments.day3;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ * @author Brent Yurek
+ *
+ * Takes first argument as a directory (or prompts for one if none given) and lists everything in the directory.
+ */
+public class ListDir {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Path dir;
+		if (args.length > 0) {
+			dir = Paths.get(args[0]);
+		} else {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Directory to list contents of: ");
+			String p = input.next();
+			dir = Paths.get(p);
+		}
+		
+		try (DirectoryStream<Path> contents = Files.newDirectoryStream(dir)){
+			for (Path item : contents) {
+				System.out.println(item.getFileName());
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR: Not a valid directory.");
+		}
+	}
+
+}
