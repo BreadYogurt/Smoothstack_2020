0774266982195186a1c8da14c13cf22de11b9c41
Brent Yurek <brentagainagain@gmail.com>
2020-12-27 14:15:18 -0600

    BookDAO wasn't committed properly, fixed

--- Smoothstack_LMS/src/com/smoothstack/lms/daos/BookDAO.java
+++ Smoothstack_LMS/src/com/smoothstack/lms/daos/BookDAO.java
@@ -5,9 +5,12 @@ package com.smoothstack.lms.daos;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.smoothstack.lms.beans.Author;
 import com.smoothstack.lms.beans.Book;
+import com.smoothstack.lms.beans.Publisher;
 
 /**
  * @author Brent Yurek
@@ -17,32 +20,52 @@ public class BookDAO extends BaseDAO<Book> {
 
 	@Override
 	List<Book> extractData(ResultSet rs) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Book> books = new ArrayList<>();
+		while (rs.next()) {
+			Book book = new Book(rs.getInt("bookId"), rs.getString("title"));
+			int authorId = rs.getInt("authId");
+			if (!rs.wasNull()) {
+				book.setAuthor(new Author(authorId, rs.getString("authorName")));
+			}
+			int publisherId = rs.getInt("pubId");
+			if (!rs.wasNull()) {
+				book.setPublisher(new Publisher(publisherId, rs.getString("publisherName"), rs.getString("publisherAddress"), rs.getString("publisherPhone")));
+			}
+		}
+		return books;
 	}
 
 	@Override
 	public void add(Book book) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		
+		save("insert into tbl_book (title, authId, pubId) values (?, ?, ?)",
+				new Object[] {
+						book.getTitle(), 
+						((book.getAuthor() != null) ? book.getAuthor().getId() : null),
+						((book.getPublisher() != null) ? book.getPublisher().getId() : null)
+				});
 	}
 
 	@Override
 	public void update(Book book) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		
+		save("update tbl_book set title = ?, authId = ?, pubId = ? where bookId = ?",
+				new Object[] {
+						book.getTitle(), 
+						((book.getAuthor() != null) ? book.getAuthor().getId() : null),
+						((book.getPublisher() != null) ? book.getPublisher().getId() : null),
+						book.getId()
+				});
 	}
 
 	@Override
 	public void delete(Book book) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		
+		save("delete from tbl_book where bookId = ?", new Object[] { book.getId() });
 	}
 
 	@Override
 	public List<Book> getAll() throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		return read("select * from tbl_book "
+				+ "left join tbl_author on tbl_book.authId = tbl_author.authorId "
+				+ "left join tbl_publisher on tbl_book.pubId = tbl_publisher.publisherId", null);
 	}
 
 }
