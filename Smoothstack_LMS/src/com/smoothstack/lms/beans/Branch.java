5ee620e86d877913188c5348564566ac914aa7ee
Brent Yurek <brentagainagain@gmail.com>
2020-12-27 10:29:04 -0600

    Implemented hashCode and equals on branch object.

--- Smoothstack_LMS/src/com/smoothstack/lms/beans/Branch.java
+++ Smoothstack_LMS/src/com/smoothstack/lms/beans/Branch.java
@@ -21,6 +21,27 @@ public class Branch {
 	private int id;
 	private String name, address;
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Branch other = (Branch) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 	public int getId() {
 		return id;
 	}
